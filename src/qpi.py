2b0c59276c0049d68b5d8e6535bc5623f2db395b
fukushima-ilab <masa1063fuk@gmail.com>
2024-01-19 16:54:09 +0900

    resolve xy reversion prob

--- src/qpi.py
+++ src/qpi.py
@@ -9,7 +9,9 @@ except:
 
 
 class QPIParameters:
-    def __init__(self, wavelength, NA, img_shape, img_center, pixelsize, offaxis_center):
+    def __init__(
+        self, wavelength, NA, img_shape, img_center, pixelsize, offaxis_center
+    ):
         self.wav = wavelength
         self.NA = NA
         self.img_shape = img_shape
@@ -22,7 +24,6 @@ class QPIParameters:
         self.freq_per_pixel = 1 / (self.pixelsize * self.dim)
         self.aperturesize = 2 * round(self.NA / self.wav / self.freq_per_pixel) + 1
 
-        
     def print_all_parameters(self):
         print(f"{self.dim=}")
         print(f"{self.freq_per_pixel=}")
@@ -33,7 +34,6 @@ class QPIParameters:
         print(f"{self.img_shape=}")
         print(f"{self.NA=}")
         print(f"{self.wav=}")
-
 
 
 def make_disk(center, radius, array_shape, highpass=False):
@@ -50,7 +50,9 @@ def make_disk(center, radius, array_shape, highpass=False):
     """
     if isinstance(array_shape, int):
         array_shape = (array_shape, array_shape)
-    xx, yy = xp.meshgrid(xp.arange(array_shape[0]), xp.arange(array_shape[1]), indexing="xy")
+    xx, yy = xp.meshgrid(
+        xp.arange(array_shape[0]), xp.arange(array_shape[1]), indexing="ij"
+    )
     circle = (xx - center[0]) ** 2 + (yy - center[1]) ** 2
     if highpass:
         disk = circle > radius**2
@@ -65,16 +67,28 @@ def qpi(array, reference, params):
     mask = make_disk(params.off_axis, params.aperturesize / 2, params.img_shape)
     reference_fft = reference_fft * mask
     reference_fft = reference_fft[
-        params.off_axis[1] - params.aperturesize // 2 : params.off_axis[1] + params.aperturesize // 2 + 1,
-        params.off_axis[0] - params.aperturesize // 2 : params.off_axis[0] + params.aperturesize // 2 + 1,
+        params.off_axis[0]
+        - params.aperturesize // 2 : params.off_axis[0]
+        + params.aperturesize // 2
+        + 1,
+        params.off_axis[1]
+        - params.aperturesize // 2 : params.off_axis[1]
+        + params.aperturesize // 2
+        + 1,
     ]
     reference = xp.fft.ifft2(xp.fft.ifftshift(reference_fft))
 
     array_fft = xp.fft.fftshift(xp.fft.fft2(array))
     array_fft = array_fft * mask
     array_fft = array_fft[
-        params.off_axis[1] - params.aperturesize // 2 : params.off_axis[1] + params.aperturesize // 2 + 1,
-        params.off_axis[0] - params.aperturesize // 2 : params.off_axis[0] + params.aperturesize // 2 + 1,
+        params.off_axis[1]
+        - params.aperturesize // 2 : params.off_axis[0]
+        + params.aperturesize // 2
+        + 1,
+        params.off_axis[0]
+        - params.aperturesize // 2 : params.off_axis[1]
+        + params.aperturesize // 2
+        + 1,
     ]
     array = xp.fft.ifft2(xp.fft.ifftshift(array_fft))
 

f0cd5bb4ac2a281e2751bc3cea5e79292cc75f46
Masato Fukushima <masa1063fuk@gmail.com>
2024-03-24 17:53:31 +0900

    Add cfg (#10)
    * refactor files
    
    * packaging
    
    * update requirements
    
    * refactor qpi
    
    * rename: src->muscopy
    
    * change: opt for appropriate type
    
    * mod: delete option in mypy
    
    * add type hint
    
    * update: docstring
    
    * refactor: aperture synthesis
    
    * add cfg

--- muscopy/cfg.py
+++ muscopy/cfg.py
@@ -1,1 +1,13 @@
 EDGE_SIZE = 2
+OFFSET_REGS = [((0, 0), (0, 0))]
+
+
+def set_edge_size(size: int):
+    global EDGE_SIZE
+    EDGE_SIZE = size
+
+
+def set_offset_regs(offset_regs: list[tuple[tuple[int, int], tuple[int, int]]]):
+    global OFFSET_REGS
+    OFFSET_REGS = offset_regs
+

e500165e53e7ba5daab9f26733f42020beccd9d8
Masato Fukushima <masa1063fuk@gmail.com>
2025-04-17 14:59:38 +0800

    add example of QPI reconstruction

---
+++ examples/qpi_reconstruct.py
@@ -0,0 +1,64 @@
+"""Demonstration of Quantitative Phase Imaging (QPI) reconstruction."""
+
+# %%
+# import modules
+
+
+from muscopy.backend_manager import BackendManager
+from muscopy.qpi import QPIParameters, make_disk, print_qpi_all_parameters, qpi
+
+# %%
+# set QPI parameters
+
+params = QPIParameters(
+    na=1.0,
+    wavelength_m=500e-9,
+    img_size_px=512,
+    px_size_m=5e-6,
+    n_sol=1.33,
+)
+
+# show QPI parameters
+print_qpi_all_parameters(params, show_properties=True)
+
+# %%
+# specify the backend
+bmg = BackendManager()
+bmg.use_numpy()
+
+print(f"{bmg.backend} backend is used.")
+backend = bmg.get_backend()
+
+# %%
+# create a hologram
+off_axis_center = (256, 256)
+
+radius = 50
+sample_disk = make_disk(backend, params.img_center, radius, params.img_size_px)
+sample_array = backend.exp(2j * backend.pi * sample_disk)
+low_pass = make_disk(backend, (params.img_center), params.aperturesize_px // 2, params.img_size_px)
+sample_array = backend.fft.ifft2(backend.fft.ifftshift(backend.fft.fftshift(backend.fft.fft2(sample_array)) * low_pass))
+sample_array /= backend.sum(backend.abs(sample_array) ** 2) ** 0.5
+
+xx, yy = backend.meshgrid(
+    backend.arange(params.img_size_px),
+    backend.arange(params.img_size_px),
+    indexing="ij",
+)
+ref_array = backend.exp(
+    2j
+    * backend.pi
+    * (xx / (off_axis_center[0] - params.img_center[0]) + yy / (off_axis_center[1] - params.img_center[1]))
+)
+ref_array /= backend.sum(backend.abs(ref_array) ** 2) ** 0.5
+
+hologram = backend.sum(sample_array + ref_array) ** 2
+
+ref_sample_array = backend.ones_like(sample_array)
+ref_sample_array /= backend.sum(backend.abs(ref_sample_array) ** 2) ** 0.5
+ref_hologram = backend.sum(ref_sample_array + ref_array) ** 2
+
+# %%
+# Extract phase of scattering wave with QPI
+
+phase_image = qpi(backend, hologram, ref_hologram, params, [off_axis_center])

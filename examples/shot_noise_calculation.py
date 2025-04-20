d00174ba7f457450e8847bd6eba049ca64594746
fukushima-ilab <masa1063fuk@gmail.com>
2025-04-20 16:11:03 +0900

    update shot noise calculation

--- examples/shot_noise_calculation.py
+++ examples/shot_noise_calculation.py
@@ -1,41 +1,88 @@
-# %%
-import os
-
-os.environ["MUSCOPY_GPU"] = "False"
-
-from ilabvis import CursorVisualizer
-from PIL import Image
-
-import muscopy as mus
-
-if mus.cfg._cp:
-    import cupy as xp
-else:
-    import numpy as xp
-
+"""Demonstration of the shot noise calculation from a hologram."""
 
 # %%
-hologram_path = r"EDIT PATH"
 
-params = mus.QPIParameters(
-    wavelength=532e-9,
-    NA=1.2,
-    img_shape=(1024, 1024),
-    pixelsize=3.45e-6 * 3 / 250,
-    offaxis_center=(309, 244),
+import matplotlib.pyplot as plt
+
+from muscopy.backend_manager import BackendManager
+from muscopy.phase_noise import calc_phase_noise, calc_visibility
+from muscopy.qpi import QPIParameters, make_disk, print_qpi_all_parameters
+
+# %%
+# config
+SHOW_IMAGE = True
+
+# Image sensor parameters
+full_well_capacity = int(10e3)  # full well capacity (e-)
+bit_depth = 12  # bit depth of the camera
+sensor_noise = 10  # sensor noise (e-)
+
+# %%
+# generate a hologram
+
+params = QPIParameters(
+    na=0.8,
+    wavelength_m=500e-9,
+    img_size_px=512,
+    px_size_m=5e-6 / 60,
     n_sol=1.33,
 )
-params.print_all_parameters()
+print_qpi_all_parameters(params, show_properties=True)
 
 # %%
-hologram = xp.array(Image.open(hologram_path))
-visibility = mus.qpi.calc_visibility(hologram, params)
-phase_noise = mus.qpi.calc_phase_noise(hologram, params)
+# specify the backend
+bmg = BackendManager()
+bmg.use_numpy()
+
+print(f"{bmg.backend} backend is used.")
+backend = bmg.get_backend()
+
+# %%
+# create a hologram
+off_axis_center = (100, 100)
+
+radius = 30
+sample_disk = make_disk(backend, params.img_center, radius, params.img_size_px)
+sample_array = backend.exp(2j * backend.pi / 3 * sample_disk)
+low_pass = make_disk(backend, (params.img_center), params.aperturesize_px // 2, params.img_size_px)
+sample_array = backend.fft.ifft2(backend.fft.ifftshift(backend.fft.fftshift(backend.fft.fft2(sample_array)) * low_pass))
+sample_array /= backend.sum(backend.abs(sample_array) ** 2) ** 0.5
+
+xx, yy = backend.meshgrid(
+    backend.arange(params.img_size_px),
+    backend.arange(params.img_size_px),
+    indexing="ij",
+)
+ref_array = backend.exp(
+    -2j
+    * backend.pi
+    * (
+        xx * (off_axis_center[0] - params.img_center[0]) / (params.img_size_px)
+        + yy * (off_axis_center[1] - params.img_center[1]) / (params.img_size_px)
+    )
+)
+ref_array /= backend.sum(backend.abs(ref_array) ** 2) ** 0.5
+
+hologram = backend.abs(sample_array + ref_array) ** 2
+# normalize
+hologram /= backend.max(hologram)  # normalize to 1
+hologram *= 2**bit_depth
+
+# %%
+# calculate visibility and phase noise
+visibility = calc_visibility(backend, hologram, params, off_axis_center)
+phase_noise = calc_phase_noise(backend, hologram, params, off_axis_center, full_well_capacity, bit_depth, sensor_noise)
 
 
 # %%
-CursorVisualizer(hologram).run()
-CursorVisualizer(visibility).run()
-CursorVisualizer(phase_noise).run()
+if SHOW_IMAGE:
+    plt.imshow(visibility)
+    plt.title("Visibility")
+    plt.colorbar()
+    plt.show()
+    plt.imshow(phase_noise)
+    plt.title("Phase noise")
+    plt.colorbar()
+    plt.show()
 
 # %%

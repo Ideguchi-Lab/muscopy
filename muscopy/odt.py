7297072fb40cc0912007b90dda3b348d21e259c0
fukushima-ilab <masa1063fuk@gmail.com>
2025-04-22 14:30:13 +0900

    create ODTParameter class

---
+++ muscopy/odt.py
@@ -0,0 +1,103 @@
+"""Optical Diffraction Tomography (ODT) calculator."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from functools import cached_property
+
+from tqdm import tqdm
+
+from muscopy.backend_manager import ArrayProtocol, BackendManager
+from muscopy.qpi import QPIParameters, make_disk, correct_offset, crop_array
+from muscopy.qpi_utils import unwrap_phase
+
+@dataclass
+class ODTParameters(QPIParameters):
+    na_illumination: float = 1.0
+
+    def verify_parameters(self) -> None:
+        """Verify the parameters.
+
+        Raises
+        ------
+        ValueError
+            1. If the NA of illumination is negative.
+            2. If the NA of illumination is greater than the NA of the objective.
+            3. If the NA of illumination is greater than the NA of the solvent.
+        """
+        super().verify_parameters()
+        if self.na_illumination < 0:
+            msg = "NA of illumination cannot be negative."
+            raise ValueError(msg)
+        if self.na_illumination > self.na:
+            msg = "NA of illumination cannot be greater than NA of the objective."
+            raise ValueError(msg)
+        if self.na_illumination > self.n_sol:
+            msg = "NA of illumination cannot be greater than NA of the solvent."
+            raise ValueError(msg)
+
+    @cached_property
+    def light_freq_lateral_px(self) -> float:
+        """Calculate the lateral frequency in pixels.
+
+        Returns
+        -------
+        `float`
+            The lateral frequency in pixels.
+        """
+        return self.light_freq_px * self.na_illumination / self.n_sol
+
+    @cached_property
+    def light_freq_axial_px(self) -> float:
+        """Calculate the axial frequency in pixels.
+
+        Returns
+        -------
+        `float`
+            The axial frequency in pixels.
+        """
+        return (self.light_freq_px ** 2 - self.light_freq_lateral_px ** 2) ** 0.5
+
+    @cached_property
+    def freq_axial_extent_px(self) -> int:
+        """Calculate the axial extent of Fourier space in pixels.
+
+        Returns
+        -------
+        `int`
+            The axial extent of Fourier space in pixels.
+        """
+        return 2 * int(self.light_freq_px) + 1
+
+    @cached_property
+    def imgpx_axial_m_per_px(self) -> float:
+        """Calculate the axial pixel size in meters.
+
+        Returns
+        -------
+        `float`
+            The axial pixel size in meters.
+        """
+        return 1 / (self.freq_per_px * self.freq_axial_extent_px)
+
+    @cached_property
+    def spectrum2field_z(self) -> float:
+        """Axial Fourier factor from spectrum to field.
+
+        Returns
+        -------
+        `float`
+            factor from spectrum to field.
+        """
+        return (self.freq_per_px / self.imgpx_axial_m_per_px) ** 0.5
+
+    @cached_property
+    def field_z2spectrum(self) -> float:
+        """Axial Fourier factor from spectrum to field.
+
+        Returns
+        -------
+        `float`
+            factor from field to spectrum.
+        """
+        return (self.imgpx_axial_m_per_px / self.freq_per_px) ** 0.5

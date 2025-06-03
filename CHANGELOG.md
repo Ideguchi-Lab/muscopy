986a90345c5b21a23b0eb71b63730e040d6e5a73
Masato Fukushima <masa1063fuk@gmail.com>
2025-06-03 11:08:11 +0900

    Reduce supporting environment (#72)
    * reduce supporting py versions
    
    * update ruff ci
    
    * udpate changelog
    
    * make it install types-tqdm
    
    * fix changelog
    
    * make ty ci to install doc libraries
    
    * ignore untyped call for skimage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,8 @@
 ### Removed
 
 - `backend_manager` module [[#53](https://github.com/Ideguchi-Lab/muscopy/issues/53)]
+- Removed support for `py39`, `py310`.
+- Removed support for `macOS`.
 
 ---
 
--- examples/unwrap_phase_demo.py
+++ examples/unwrap_phase_demo.py
@@ -101,7 +101,7 @@ if SHOW_IMAGE:
 
 phase_image_np = np.asarray(phase_image)
 
-unwrapped_skimage: NDArray[np.float64] = skimage_unwrap_phase(phase_image_np)
+unwrapped_skimage: NDArray[np.float64] = skimage_unwrap_phase(phase_image_np)  # type: ignore[no-untyped-call]
 
 if SHOW_IMAGE:
     plt.imshow(unwrapped_skimage)

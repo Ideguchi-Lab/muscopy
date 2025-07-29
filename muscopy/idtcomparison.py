a4ba3cc4b92cd82521586b20784b6625a7a2d60a
patsuko <Patsukowonder@gmail.com>
2025-07-29 15:31:32 +0900

    created new comparison file

---
+++ muscopy/idtcomparison.py
@@ -0,0 +1,7 @@
+"""IDT Comparison
+
+Here we compare a digitally developed ground truth sphere of various sizes
+and reconstruct it using IDT. We then compare the result to the original to see
+for what sizes and what refractive index IDT is most effective
+"""
+
